231894bb4a71edb9973265f9c84e2d7dde9e714d
Gajender <Iamsanju0707@gmail.com>
2024-12-31 11:34:19 +0530

    adding the data to mongodb

--- push_data.py
+++ push_data.py
@@ -6,7 +6,8 @@ from src.logging.logging import Logger
 from src.exception.exception import CustomException
 from pathlib import Path
 import json
-import pymongo
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
 
 
 #loading the Env Variables...
@@ -21,6 +22,7 @@ class NetworkDataExtraction:
                  config_file_path=config_file_path,mongodb_uri=mongodb_uri
             ):
         try:
+            self.mongodb_uri=mongodb_uri
             self.database_name=database_name
             self.collection_name=collection_name
             yaml_file_data=read_yaml(config_file_path)
@@ -32,7 +34,8 @@ class NetworkDataExtraction:
     def csv_to_json_convertor(self):
         try:
             data=pd.read_csv(self.data_path)
-            records=data.to_json(orient="records")
+            records=data.to_dict(orient="records")
+            # print(type(records))
             return records
         except Exception as e:
             raise CustomException(e)
@@ -41,11 +44,17 @@ class NetworkDataExtraction:
     def push_to_mongodb(self):
         try:
             records=self.csv_to_json_convertor()
-            print(records)
+            # client = MongoClient(self.mongodb_uri, server_api=ServerApi('1'))
+            client=MongoClient(self.mongodb_uri)
+            database=client[self.database_name]
+            collection=database[self.collection_name]
+            collection.insert_many(records)
+            Logger.info("Data Push to MongoDB Sucessfully...")
+
         except Exception as e:
             raise CustomException(e)
 
 
 if __name__=="__main__":
-    obj=NetworkDataExtraction(database_name="Network Security Database",collection_name="NetworkData")
+    obj=NetworkDataExtraction(database_name="Network_Security_Database",collection_name="NetworkData")
     obj.push_to_mongodb()
